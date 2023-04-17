5df784813078cb94088879cf47281894825738d3
Roman Polyansky <roman_polyan@mail.ru>
2023-04-18 00:43:27 +0300

    fixing github tests

--- stats-service/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.practicum;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.practicum.request.entity.Request;
 
