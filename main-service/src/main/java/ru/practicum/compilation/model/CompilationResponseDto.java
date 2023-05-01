672cf4e82ee869ad17cd6d72f0d92061222f2613
Roman Polyansky <roman_polyan@mail.ru>
2023-05-01 15:55:26 +0300

    fighting git tests

--- main-service/src/main/java/ru/practicum/compilation/model/CompilationResponseDto.java
+++ main-service/src/main/java/ru/practicum/compilation/model/CompilationResponseDto.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Data
 public class CompilationResponseDto {
+    private Long id;
     private String title;
     private List<EventResponseShortDto> events;
     private Boolean pinned;
